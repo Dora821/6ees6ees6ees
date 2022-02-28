9c34139803123d4e664784df69fc256f9e59ccfc
Dora Xia <duruoxia1992@gmail.com>
2022-02-28 12:07:08 -0800

    finished HoneyMakerBee class

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,18 @@
-class HoneyMakerBee {
+class HoneyMakerBee extends Bee {
   // TODO..
-};
+  constructor(...args) {
+    super(...args);
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+    // this.food = Grub.proptotype.food;
+  }
+  makeHoney () {
+    this.honeyPot += 1;
+    return this.honeyPot;
+  }
+  giveHoney () {
+    this.honeyPot -= 1;
+    return this.honeyPot;
+  }
+}
